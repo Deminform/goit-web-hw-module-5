2127dfa7b3c7f1b3899cdff09f93ccf1cafe3716
Deminform <deminform@gmail.com>
2024-09-08 17:32:46 +0200

    0.1.5

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import sys
-import platform
-import aiohttp
 import asyncio
+import platform
+import sys
 from datetime import date, timedelta
 
+import aiohttp
 
-LINK_TEMPLATE = 'https://api.privatbank.ua/p24api/exchange_rates?date={}'
+LINK_TEMPLATE = "https://api.privatbank.ua/p24api/exchange_rates?date={}"
 MAX_HISTORY_DAYS = 10
 
-currencies_list = ['EUR', 'USD']
+currencies_list = ["EUR", "USD"]
 history_days = 1
 
 
 async def parse_response(json_response, currencies: list):
     parsed_dict = {}
-    date_from_response = json_response['date']
-    exchange_rate_list = json_response['exchangeRate']
-    parsed_currencies_list = [el for el in exchange_rate_list if el['currency'] in currencies]
+    date_from_response = json_response["date"]
+    exchange_rate_list = json_response["exchangeRate"]
+    parsed_currencies_list = [
+        el for el in exchange_rate_list if el["currency"] in currencies
+    ]
     for el in parsed_currencies_list:
-        parsed_dict.update({el['currency']: {'sale': el['saleRate'], 'purchase': el['purchaseRate']}})
+        parsed_dict.update(
+            {el["currency"]: {"sale": el["saleRate"], "purchase": el["purchaseRate"]}}
+        )
     return {date_from_response: parsed_dict}
 
 
@@ -29,18 +33,18 @@ async def get_response(session, url, currencies: list):
                 json_result = await response.json()
                 return await parse_response(json_result, currencies)
     except aiohttp.ClientConnectorError as err:
-        print(f'Connection error: {url}', str(err))
+        print(f"Connection error: {url}", str(err))
     except aiohttp.ClientResponseError as err:
-        print(f'Server response error: {url}', str(err))
+        print(f"Server response error: {url}", str(err))
     except asyncio.TimeoutError:
-        print(f'Request timed out for {url}')
+        print(f"Request timed out for {url}")
 
 
 async def make_links(number_of_days):
     list_of_links = []
     for i in range(number_of_days):
         day = date.today() - timedelta(days=i)
-        list_of_links.append(LINK_TEMPLATE.format(day.strftime('%d.%m.%Y')))
+        list_of_links.append(LINK_TEMPLATE.format(day.strftime("%d.%m.%Y")))
     return list_of_links
 
 
@@ -54,13 +58,21 @@ async def main(currencies: list, number_of_days=1):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1].isdigit() and int(sys.argv[1]) <= MAX_HISTORY_DAYS:
+    if (
+        len(sys.argv) == 2
+        and sys.argv[1].isdigit()
+        and int(sys.argv[1]) <= MAX_HISTORY_DAYS
+    ):
         history_days = int(sys.argv[1])
-    elif len(sys.argv) >= 3 and sys.argv[1].isdigit() and int(sys.argv[1]) <= MAX_HISTORY_DAYS:
+    elif (
+        len(sys.argv) >= 3
+        and sys.argv[1].isdigit()
+        and int(sys.argv[1]) <= MAX_HISTORY_DAYS
+    ):
         _, history_days, *currencies_args = sys.argv
         currencies_list.extend(currencies_args)
 
-    if platform.system() == 'Windows':
+    if platform.system() == "Windows":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     request_result = asyncio.run(main(currencies_list, int(history_days)))
--- web_server/server.py
+++ web_server/server.py
@@ -2,31 +2,31 @@ import asyncio
 import logging
 from datetime import datetime
 
-import websockets
 import names
+import websockets
 from aiofile import async_open
 from aiopath import AsyncPath
-
-from main import main as currency_exchange_main
 from websockets import WebSocketServerProtocol
 from websockets.exceptions import ConnectionClosedOK
 
+from main import main as currency_exchange_main
+
 logging.basicConfig(level=logging.INFO)
-log_path = AsyncPath('exchange_request_log.txt')
+log_path = AsyncPath("exchange_request_log.txt")
 
 
 class Server:
     clients = set()
-    BASE_CURRENCIES = ['EUR', 'USD']
+    BASE_CURRENCIES = ["EUR", "USD"]
 
     async def register(self, ws: WebSocketServerProtocol):
         ws.name = names.get_full_name()
         self.clients.add(ws)
-        logging.info(f'{ws.remote_address} connects')
+        logging.info(f"{ws.remote_address} connects")
 
     async def unregister(self, ws: WebSocketServerProtocol):
         self.clients.remove(ws)
-        logging.info(f'{ws.remote_address} disconnects')
+        logging.info(f"{ws.remote_address} disconnects")
 
     async def send_to_clients(self, message: str):
         if self.clients:
@@ -44,7 +44,7 @@ class Server:
 
     async def distribute(self, ws: WebSocketServerProtocol):
         async for message in ws:
-            if message.split(' ', 1)[0] == 'exchange':
+            if message.split(" ", 1)[0] == "exchange":
                 result = await self.parse_command(message)
                 log_msg = message
                 message = await currency_exchange_main(result[0], result[1])
@@ -54,11 +54,11 @@ class Server:
 
     async def parse_command(self, message: str):
         new_list = self.BASE_CURRENCIES.copy()
-        if len(message.split(' ')) == 2 and message.split(' ')[1].isdigit():
-            _, days = message.split(' ')
+        if len(message.split(" ")) == 2 and message.split(" ")[1].isdigit():
+            _, days = message.split(" ")
             return new_list, int(days)
-        elif len(message.split(' ')) >= 3 and message.split(' ')[1].isdigit():
-            _, days, *currency = message.split(' ')
+        elif len(message.split(" ")) >= 3 and message.split(" ")[1].isdigit():
+            _, days, *currency = message.split(" ")
             new_list.extend(currency)
             return new_list, int(days)
         else:
@@ -69,27 +69,30 @@ class Server:
         result = []
         for record in message:
             for date, currencies in record.items():
-                result.append(f'<br>{date}')
+                result.append(f"<br>{date}")
                 for currency, rates in currencies.items():
-                    sale = rates['sale']
-                    purchase = rates['purchase']
-                    result.append(f"{currency} : sale {"{:.2f}".format(sale)}, purchase {"{:.2f}".format(purchase)} |")
+                    sale = rates["sale"]
+                    purchase = rates["purchase"]
+                    result.append(
+                        f"{currency} : sale {"{:.2f}".format(sale)}, purchase {"{:.2f}".format(purchase)} |"
+                    )
         return "<br>".join(result)
 
     @staticmethod
     async def make_log(name, message, response):
-        async with async_open(log_path, 'a') as afp:
+        async with async_open(log_path, "a") as afp:
             await afp.write(
                 f'{name}: made a request at: {datetime.now().strftime("%m/%d/%Y %H:%M:%S")} '
-                f'/ request is: {message} '
-                f'/ response is: {response}\n')
+                f"/ request is: {message} "
+                f"/ response is: {response}\n"
+            )
 
 
 async def main():
     server = Server()
-    async with websockets.serve(server.ws_handler, 'localhost', 8080):
+    async with websockets.serve(server.ws_handler, "localhost", 8080):
         await asyncio.Future()  # run forever
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
